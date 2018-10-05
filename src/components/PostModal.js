dd291843bef18e6129310e9da458286b54718216
sofia-aguirre <sofiaxaguirre@gmail.com>
2018-10-05 12:22:33 -0700

    add-post modal fuctional

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -1,39 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Modal from 'react-modal';
-// import './.css';
 
-// npm install react-modal
-// https://www.npmjs.com/package/react-modal#examples
-// https://github.com/reactjs/react-modal/blob/a2c38cc1032500ac697bf1d1d68dbb256b7fb097/docs/README.md
-
-// setAppElement() - This is needed so screen readers don't see main content when modal is opened
-//  #root is the id in index.html
-// functional component instead of class component
-// Modal require to pass in some props to work
-// isOpen - Boolean describing if the modal should be shown or not
-// contentLabel - String indicating how the content container should be announced to screenreaders
-// onRequestClose - Function that will be run when the modal is requested to be closed (either by clicking on overlay or pressing ESC)
-// className and overlayClassName apply style from LoginModal.css to this Modal
-// multiple input form need to have name attribute to identify which state
 Modal.setAppElement('#root')
-const PostModal = (props) => {
+class PostModal extends Component {
+    render () {
     return (
         <Modal
-        isOpen={props.modalIsOpen}
+        isOpen={this.props.modalIsOpen}
         contentLabel="Example Modal"
-        onRequestClose={props.handleCloseModal}
+        onRequestClose={this.props.handleCloseModal}
         className="Modal"
         overlayClassName="Overlay"
         >
-            <form onSubmit={props.handleSubmit}>
-                <input type="text" name='input1' placeholder="Title" onKeyUp={props.handleChange}/>
-                <textarea type="text" name='input2' placeholder="Tell us what you love about this city.." onKeyUp={props.handleChange}></textarea>
-                {/* <input type="text" name='input3' onKeyUp={props.handleChange}/> */}
-                <input type="submit"  value="Post" />
+        ADD POST
+            <form id="addPost">
+                <input type="text" name='title' placeholder="Title" onChange={this.props.handleInput}/>
+                <input type="text" name='body' placeholder="Body" onChange={this.props.handleInput}/>
+                <input type="image" />
+                <input type="submit"  value="Add Post" onClick={this.props.handleAddPost}/>
             </form>
-            <button onClick={props.handleCloseModal}> x </button>
+
+            <button onClick={this.props.handleCloseModal}> x </button>
         </Modal>
     )
-};
+}
+}
 
 export default PostModal;
+
+
