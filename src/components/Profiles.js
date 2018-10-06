349c2f303ace9f081009866aaa08a5db05455328
Andrea  Piazza <andrea@andreas-mbp.attlocal.net>
2018-10-06 12:55:55 -0700

    Rendered Username, Current City, and Member Since on profile page.

--- src/components/Profiles.js
+++ src/components/Profiles.js
@@ -6,44 +6,29 @@ class Profiles extends Component {
     constructor(){
         super()
         this.state = {
-          user: [],
+          user: {},
         }
       }
 
-    //   componentDidMount = async() => {
-    //     UserModel.getInfo(localStorage.getItem("username"))
-    //     .then(res =>{
-    //         console.log(res)
-    //         this.setState({user:res.data})
-    //     })
-    //     .catch(err =>{
-    //         console.log("Profile Catch",err)
-    //     })
+    componentDidMount = () =>{
+        console.log('Profile Mounted')
 
-    //     componentDidMount = async () =>{
-
-    //     }
-    //       const name  = this.params.name
-    //     console.log(`here is the id`, name)
-    //     await fetch 
-    //     (`http://localhost:3001/users/${name}`)
-    
-    //     .then(response => response.json())
-    //     .then(info => this.setState({user : [info.data]} ))
-    //     .catch(err => error)
-        
-    //     console.log('here is the state ',this.state.user);
-    //     console.log("NAME" + this.props.name)
-    //   }
-
+        UserModel.getInfo(this.props.match.params.name)
+            .then(response=>{
+                this.setState({user: response.data})
+            })
+            .catch(err =>{
+                console.log("Profile did not mount" + err);
+            })
+      }
   render() {
     return (
         <div className ="userProfInfo">
             <div className="userName">
-                <img className="profilePic" src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg" />
-                <h2>Username{this.state.name}</h2>
-                <h2>Current City:{this.state.currentCity}</h2>
-                <h2>Member Since:{this.state.dateJoined} </h2>
+                <img className="profilePic"  alt ="" src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg" />
+                <h2>Username: {this.props.match.params.name}</h2>
+                <h2>Current City:{this.state.user.currentCity}</h2>
+                <h2>Member Since:{this.state.user.dateJoined} </h2>
             </div>
             <UserPostsList/>
         </div>
--- src/models/User.js
+++ src/models/User.js
@@ -1,14 +1,11 @@
 import axios from 'axios'
 
 class UserModel{
-    //class function to get information
-    static getInfo(username){
-        let url = `http://localhost3001/users/${username}`
-        let request = axios.get(url)
-        console.log(url)
-        return request
-
-    }
+    static getInfo (username) {
+        let url = `http://localhost:3001/users/${username}`;
+        let req = axios.get(url);
+        return req;
+      }
 }
 
 export default UserModel
